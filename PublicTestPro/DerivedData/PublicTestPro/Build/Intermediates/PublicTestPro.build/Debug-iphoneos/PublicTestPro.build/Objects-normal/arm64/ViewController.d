097fa25bcc4770827ba6cc6d80a0bbbe3ec8f2fe
lilei <lilei@192.168.2.107>
2016-02-29 21:24:45 +0800

    删除了PublicTestPro

--- PublicTestPro/DerivedData/PublicTestPro/Build/Intermediates/PublicTestPro.build/Debug-iphoneos/PublicTestPro.build/Objects-normal/arm64/ViewController.d
+++
@@ -1,9 +0,0 @@
-dependencies: \
-  /Users/lilei/ios_projects/LLDownloader/PublicTestPro/PublicTestPro/ViewController.m \
-  /Users/lilei/ios_projects/LLDownloader/PublicTestPro/PublicTestPro/ViewController.h \
-  /Users/lilei/ios_projects/LLDownloader/LLDownloader/LLDownloader.h \
-  /Users/lilei/ios_projects/LLDownloader/LLDownloader/LLHttpDownloader.h \
-  /Users/lilei/ios_projects/LLDownloader/LLDownloader/LLFileOperator.h \
-  /Users/lilei/ios_projects/LLDownloader/LLDownloader/LLLoger.h \
-  /Users/lilei/ios_projects/LLDownloader/LLDownloader/LLNetworkState.h \
-  /Users/lilei/ios_projects/LLDownloader/LLDownloader/LLDownloadBar.h
--- PublicTestPro/DerivedData/PublicTestPro/Build/Intermediates/PublicTestPro.build/Debug-iphoneos/PublicTestPro.build/Objects-normal/arm64/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/lilei/ios_projects/LLDownloader/PublicTestPro/PublicTestPro/main.m \
-  /Users/lilei/ios_projects/LLDownloader/PublicTestPro/PublicTestPro/AppDelegate.h
